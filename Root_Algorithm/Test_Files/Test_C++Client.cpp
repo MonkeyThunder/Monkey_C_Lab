86df25dc8415d21dacebd809363daecf3e08bf6b
Monkey Thunder <lab.monkeythunder@gmail>
2018-09-21 11:27:47 +0900

    Socket Client Test address change

--- Root_Algorithm/Test_Files/Test_C++Client.cpp
+++ Root_Algorithm/Test_Files/Test_C++Client.cpp
@@ -19,7 +19,7 @@ int main(){
 
     servaddr.sin_family=AF_INET;
     servaddr.sin_port=htons(55080);
-    servaddr.sin_addr.s_addr=inet_addr("127.0.0.1");
+    servaddr.sin_addr.s_addr=inet_addr("kim-lab.net");
 
     if(connect(sock,(struct sockaddr*)&servaddr, sizeof(servaddr))<0){
         perror("Connection failed !");
