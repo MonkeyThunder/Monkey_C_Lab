2dd29d8b164fd087811caccc94810e05a9681773
Monkey Thunder <lab.monkeythunder@gmail>
2018-09-21 10:28:08 +0900

    Socket Client Test

---
+++ Root_Algorithm/Test_Files/Test_C++Client.cpp
@@ -0,0 +1,33 @@
+//
+// Created by DongHoon Kim on 21/09/2018.
+//
+
+#include <iostream>
+#include<unistd.h>
+#include <arpa/inet.h>
+
+int main(){
+
+    int sock, nbyte;
+
+    struct sockaddr_in servaddr;
+
+    if((sock = socket(PF_INET, SOCK_STREAM,0))<0){
+        perror("socket failed !");
+        return -1;
+    }
+
+    servaddr.sin_family=AF_INET;
+    servaddr.sin_port=htons(55000);
+    servaddr.sin_addr.s_addr=inet_addr("127.0.0.1");
+
+    if(connect(sock,(struct sockaddr*)&servaddr, sizeof(servaddr))<0){
+        perror("Connection failed !");
+        return -1;
+    }
+
+    puts("Connection Success !");
+    close(sock);
+
+    return 0;
+}
